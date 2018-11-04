c34cf7f133e66d3f6537f7d4195c065d99daadd4
kamrulislamcmt <44641354+kamrulislamcmt@users.noreply.github.com>
2018-11-04 12:04:16 +0600

    medical

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+(function ($) {
+	"use strict";
+	
+    jQuery(document).ready(function($){	
+		
+//        slicknav js start
+			$("ul#navigation").slicknav({
+				prependTo: ".template-responsive-wrap"
+			});
+//        slicknav js end    	
+		
+		
+		
+		
+    });	
+	
+}(jQuery));	
