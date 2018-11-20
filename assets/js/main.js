a249ed9e9a50181b410c448a122be495807befef
kamrulislamcmt <44641354+kamrulislamcmt@users.noreply.github.com>
2018-11-20 12:19:09 +0600

    dinku

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,40 @@
-jQuery(document).ready(function($) {
-	$(".slide-list").slick({
-		prevArrow: "<i class='fa fa-angle-left'></i>",
-		nextArrow: "<i class='fa fa-angle-right'></i>",
-	});
+(function ($) {
+	"use strict";
 
-	$(".course-carousel").slick({
-		slidesToShow: 3,
-		prevArrow: "<i class='fa fa-angle-left'></i>",
-		nextArrow: "<i class='fa fa-angle-right'></i>",
-	});
+    jQuery(document).ready(function($){
+		
+		$(".testimonial-content-bottom-area").owlCarousel({
+			items: 4,
+			nav: true,
+			dots: false,
+			autoplay: true,
+			autoplayTimeout: 3000,
+			loop: true,
+			navText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+		});
 
-});
+		$(".customer-content").owlCarousel({
+			items: 2,
+			nav: false,
+			dots: true,
+			autoplay: false,
+			autoplayTimeout: 3000,
+			loop: true,
+			margin: 30,
+		});
+
+
+
+
+
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+    });
+
+
+}(jQuery));	
