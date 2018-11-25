e90538967214f5d73130e194f66a0a1cad84ad25
kamrulislamcmt <44641354+kamrulislamcmt@users.noreply.github.com>
2018-11-26 01:40:21 +0600

    breakup

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,26 +2,52 @@
 	"use strict";
 
     jQuery(document).ready(function($){
-		
-		$(".testimonial-content-bottom-area").owlCarousel({
-			items: 4,
-			nav: true,
-			dots: false,
+//        slicknav js start
+			$("ul#navigation").slicknav({
+				prependTo: ".responsive-menu-wrap"
+			});
+//        slicknav js end   
+
+		$(".testimonial-content").owlCarousel({
+			items: 1,
+			nav: false,
+			dots: true,
 			autoplay: true,
 			autoplayTimeout: 3000,
 			loop: true,
-			navText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
 		});
-
-		$(".customer-content").owlCarousel({
+		
+		$(".full-customer").owlCarousel({
 			items: 2,
+			margin: 30,
 			nav: false,
 			dots: true,
-			autoplay: false,
+			autoplay: true,
 			autoplayTimeout: 3000,
 			loop: true,
-			margin: 30,
+			// navText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
 		});
+
+		$('#play-video').on('click', function(e){
+  e.preventDefault();
+  $('#video-overlay').addClass('open');
+  $("#video-overlay").append('<iframe width="560" height="315" src="https://www.youtube.com/embed/ngElkyQ6Rhs" frameborder="0" allowfullscreen></iframe>');
+});
+
+$('.video-overlay, .video-overlay-close').on('click', function(e){
+  e.preventDefault();
+  close_video();
+});
+
+$(document).keyup(function(e){
+  if(e.keyCode === 27) { close_video(); }
+});
+
+function close_video() {
+  $('.video-overlay.open').removeClass('open').find('iframe').remove();
+};
+
+
 
 
 
@@ -34,6 +60,7 @@
 
     jQuery(window).load(function(){
 
+		jQuery(".portfolio-items").isotope();
     });
 
 
